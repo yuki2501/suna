a93452d4c9fc47eac78bd02420a1b69cd5834ec3
yuki25011 <yuki2501@pm.me>
2025-12-07 02:28:12 +0900

    ビルド関係について追記

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust dev env with flakes";
+  description = "suna: sandbox-exec wrapper";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
@@ -12,16 +12,41 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        rustPlatform = pkgs.rustPlatform;
+
+        # このリポジトリ自体をビルド対象にする
+        sunaPkg = rustPlatform.buildRustPackage {
+          pname = "suna";
+          version = "0.1.0";
+          # flake のルートが Rust プロジェクトのルートという前提
+          src = ./.;
+          # Cargo.lock を使って依存を固定
+          cargoLock.lockFile = ./Cargo.lock;
+        };
       in {
+        # nix build .#suna とか nix build .#default でビルドできるようにする
+        packages = {
+          default = sunaPkg;
+          suna = sunaPkg;
+        };
+
+        # nix run . で実行できるように app も定義しておく
+        apps.default = flake-utils.lib.mkApp {
+          drv = sunaPkg;
+        };
+
+        # いままでの devShell も残す。開発中は nix develop で入る。
         devShells.default = pkgs.mkShell {
+          # ビルド時と同じ依存を共有しておくと微妙に楽
+          inputsFrom = [ sunaPkg ];
+
           buildInputs = with pkgs; [
             rustc
             cargo
             rustfmt
             clippy
             rust-analyzer
-            pkg-config
-            openssl
           ];
         };
       });
